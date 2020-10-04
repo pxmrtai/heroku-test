f718d75819e675d34ab9b1f8c4731625ca688b2a
Glitch (ls20-pagination) <none>
2020-10-04 08:19:31 +0000

    🌗🎌 Checkpoint ./views/users/profile/avatar.pug:20086806/439 ./routes/users.route.js:20086806/17 ./views/users/view.pug:20086806/18

--- routes/users.route.js
+++ routes/users.route.js
@@ -23,9 +23,9 @@ router.get('/customer',controller.customer)
 router.get('/userLogin',controller.userLogin)
 router.get('/profile/avatar/:id',controller.changeAvatar)
 router.get('/:id',controller.view)
-router.get("/index/:id/delete:",controller.delete)
+router.get("/index/:id/delete",controller.delete)
 
-router.post('/profile/avatar',upload.single('avatar'),controller.postChangeAvatar)
+router.post('/profile/avatar/:id',upload.single('avatar'),controller.postChangeAvatar)
 
 
 module.exports = router;
