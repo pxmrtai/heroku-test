38577cda4212d2da1903185ef829d6108c320910
Glitch (ls20-pagination) <none>
2020-10-04 15:02:51 +0000

    👲🔨 Checkpoint ./controller/user.controller.js:20086806/311 ./views/users/view.pug:20086806/152 ./views/layouts/common.pug:20086806/86

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -58,10 +58,14 @@ module.exports.index = (req, res) => {
 module.exports.view = (req, res) => {
   var id = req.params.id;
   var user = db.get('user').find({id: id}).value()
- 
+  var users = db.get('user').find({id: req.signedCookies.userId}).value()
+
   console.log(user)
+  console.log(users)
+
   res.render("users/view", {
-    user: user
+    user: user,
+    users: users
   });
 };
 
