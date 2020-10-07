51d35260bf9e0dbba749d638ae608eadc9c4c0cd
Glitch (ls20-pagination) <none>
2020-10-07 09:09:37 +0000

    🎀🐋 Checkpoint ./controller/user.controller.js:20086806/6143

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,7 @@
 const db = require("../db");
+require("dotenv").config();
 const shortid = require("shortid");
-var cloudinary = require('cloudinary').v2;
+var cloudinary = require("cloudinary").v2;
 
 var emailMiddleware = require("../middleware/email.middleware");
 const bcrypt = require("bcrypt");
@@ -8,18 +9,16 @@ const saltRounds = 10;
 const myPlaintextPassword = "s0//P4$$w0rD";
 const someOtherPlaintextPassword = "not_bacon";
 
-
 cloudinary.config({
-    cloud_name: "dvjjx3g49",
-    api_key: "795484751145565",
-    api_secret: "NtzjR0Rhy6Fzj7_wkExbwKKuaR0"
-  });
-
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
 
 module.exports.customer = (req, res) => {
   var listBook = db.get("list").value();
   var rentalList = db.get("rentalList").value();
-  
+
   res.render("userOnly/customer", {
     list: db.get("list").value(),
     rentalList: rentalList
@@ -44,7 +43,7 @@ module.exports.index = (req, res) => {
   var perPage = 2;
   var start = (page - 1) * perPage;
   var end = (page - 1) * perPage + perPage;
-  var maxPage =  Math.ceil(db.get("user").value().length / perPage);
+  var maxPage = Math.ceil(db.get("user").value().length / perPage);
   res.render("users/index", {
     page,
     maxPage,
@@ -57,11 +56,17 @@ module.exports.index = (req, res) => {
 };
 module.exports.view = (req, res) => {
   var id = req.params.id;
-  var user = db.get('user').find({id: id}).value()
-  var users = db.get('user').find({id: req.signedCookies.userId}).value()
+  var user = db
+    .get("user")
+    .find({ id: id })
+    .value();
+  var users = db
+    .get("user")
+    .find({ id: req.signedCookies.userId })
+    .value();
 
-  console.log(user)
-  console.log(users)
+  console.log(user);
+  console.log(users);
 
   res.render("users/view", {
     user: user,
@@ -81,38 +86,48 @@ module.exports.createUser = (req, res) => {
 };
 module.exports.changeAvatar = (req, res) => {
   var id = req.params.id;
-  console.log(id)
-  res.render("users/profile/avatar",{
-    user: db.get('user').find({id: id}).value()
+  console.log(id);
+  res.render("users/profile/avatar", {
+    user: db
+      .get("user")
+      .find({ id: id })
+      .value()
   });
 };
 module.exports.postChangeAvatar = (req, res) => {
   var id = req.params.id;
   var user = db
-      .get("user")
-      .find({ id: id })
-      .value();
-  req.body.avatar = req.file.path.split("/").slice(1).join("/");
-  db.get('user')
-  .find({ id: req.params.id })
-  .assign({ avatar: req.body.avatar})
-  .write()
-  var admin = db.get('user').find({id:req.signedCookies.userId}).value()
-  if(!admin.isAdmin){
-    return res.redirect('/users/userLogin')
+    .get("user")
+    .find({ id: id })
+    .value();
+  req.body.avatar = req.file.path
+    .split("/")
+    .slice(1)
+    .join("/");
+  db.get("user")
+    .find({ id: req.params.id })
+    .assign({ avatar: req.body.avatar })
+    .write();
+  var admin = db
+    .get("user")
+    .find({ id: req.signedCookies.userId })
+    .value();
+  if (!admin.isAdmin) {
+    return res.redirect("/users/userLogin");
   }
-  if(req.body.avatar){
-   cloudinary.uploader.upload(req.file.path)
-    .then((result) => {
-      res.status(200).send({
-        result,
+  if (req.body.avatar) {
+    cloudinary.uploader
+      .upload(req.file.path)
+      .then(result => {
+        res.status(200).send({
+          result
+        });
+      })
+      .catch(error => {
+        res.status(500).send({
+          error
+        });
       });
-    }).catch((error) => {
-      res.status(500).send({
-        error,
-      });
-    });
   }
-    res.redirect('/users/'+ id)
-
+  res.redirect("/users/" + id);
 };
