9f773c61b01d110717a3f85065238d26aa2d78b9
Glitch (ls20-pagination) <none>
2020-10-13 08:44:30 +0000

    🐮📻 Checkpoint ./middleware/session.middleware.js:20086806/686:22228234/1848 ./db.json:20086806/2760

--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -2,21 +2,31 @@ var shortId = require("shortid");
 const db = require("../db");
 
 module.exports = (req, res, next) => {
+  console.log(req.signedCookies)
   if (!req.signedCookies.sessionId) {
     var sessionId = shortId.generate();
-    res.cookie("sessisonId", sessionId, {
-      signed: true
+    res.cookie("sessionId", sessionId, { 
+      signed: true,
+      sameSite: 'None',
+      secure: true
     });
-      db
-      .get("sessions")
+    db.get("sessions")
       .push({
         id: sessionId
       })
       .write();
-    console.log('sessionId', sessionId);
   }
+  console.log('sessionId', sessionId)
   next();
 };
 //em chào anh.. em vừa nhắn trên kia đấy ạ
 // dạ.. em cảm ơn ạ
 // ok để mình xem code của bạn há
+// nãy glicth của em k set cookie vì thiếu 2 dòng kia..chuyển sang cửa sổ khác thì nó tự set :))
+// dạ em chuyển sang cửa sổ khác thì tự set;.. còn nếu ở đây thì không ạ... trang glitch tự động .. 
+// đấy là vấn đề của em đấy ạ.. khi refresh thì nó cứ tạo thêm id ạ... warm là do anh em chat ở đây nên nó 
+// tự refresh luôn ạ
+// mình thấy nó set 
+// nhưng mà mình log thì thấy nó log ra nhiều cái sessionId không biết có phải bạn refesh nên nó log không 
+// um xài cái chuyển qua cửa sổ khác ấy 
+// hừm lạ ta mình cũng không biết tại sao
