b817ce4eb373e23fda579eda74808e622f92e2ab
Glitch (ls20-pagination) <none>
2020-10-13 08:34:51 +0000

    🎅🍘 Checkpoint ./middleware/session.middleware.js:20086806/145:22228234/816 ./db.json:20086806/513 ./server.js:22228234/3975

--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -1,19 +1,22 @@
-var shortId = require('shortid')
-const db = require('../db')
+var shortId = require("shortid");
+const db = require("../db");
 
-
-module.exports = (req,res,next)=>{
-  if(!req.signedCookies.sessionId){
-    var sessionId = shortId.generate()
-     res.cookie('sessisonId', sessionId,{
-    signed: true
-  })
-   var a = db.get('sessions').push( {
-      id : sessionId
-    }).write()
+module.exports = (req, res, next) => {
+  if (!req.signedCookies.sessionId) {
+    var sessionId = shortId.generate();
+    res.cookie("sessisonId", sessionId, {
+      signed: true
+    });
+      db
+      .get("sessions")
+      .push({
+        id: sessionId
+      })
+      .write();
+    console.log('sessionId', sessionId);
   }
-  console.log(a)
-  
-  next()
-  
-}
+  next();
+};
+//em chào anh.. em vừa nhắn trên kia đấy ạ
+// dạ.. em cảm ơn ạ
+// ok để mình xem code của bạn há
