0ff0dc1bc15c5dbce9226db8c02931f3feb6df42
Glitch (ls22-session2) <none>
2020-10-13 08:46:22 +0000

    🐴🛍 Checkpoint ./middleware/session.middleware.js:20086806/635

--- middleware/session.middleware.js
+++ middleware/session.middleware.js
@@ -19,14 +19,4 @@ module.exports = (req, res, next) => {
   console.log('sessionId', sessionId)
   next();
 };
-//em chào anh.. em vừa nhắn trên kia đấy ạ
-// dạ.. em cảm ơn ạ
-// ok để mình xem code của bạn há
-// nãy glicth của em k set cookie vì thiếu 2 dòng kia..chuyển sang cửa sổ khác thì nó tự set :))
-// dạ em chuyển sang cửa sổ khác thì tự set;.. còn nếu ở đây thì không ạ... trang glitch tự động .. 
-// đấy là vấn đề của em đấy ạ.. khi refresh thì nó cứ tạo thêm id ạ... warm là do anh em chat ở đây nên nó 
-// tự refresh luôn ạ
-// mình thấy nó set 
-// nhưng mà mình log thì thấy nó log ra nhiều cái sessionId không biết có phải bạn refesh nên nó log không 
-// um xài cái chuyển qua cửa sổ khác ấy 
-// hừm lạ ta mình cũng không biết tại sao
+
