707caa4bbcd05db0043f8e45ae05a22526719155
Glitch (ls22-session2) <none>
2020-11-01 07:40:45 +0000

    👞💑 Checkpoint ./routes/transaction.route.js:20086806/149 ./views/transaction/index.pug:20086806/45

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -5,13 +5,14 @@ const shortid = require('shortid')
 const bodyParser = require('body-parser')
 var controller = require('../controller/transaction.controller')
 var emailMiddleware = require('../middleware/email.middleware')
+var multer  = require('multer')
+var upload = multer({ dest: './public/uploads/' })
 
 
 router.use(bodyParser.json()) // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ rentalList: [] })
-  .write()
+
 router.get("/:id/complete",controller.getcomplete)
 
 router.get("/index",controller.rentalIndex)
@@ -22,7 +23,7 @@ router.get("/create",controller.createRentalList)
 
 
 
-router.post('/create',emailMiddleware.notExist, controller.postCreateRentalList)
+router.post('/create',emailMiddleware.notExist,upload.single('avatar'), controller.postCreateRentalList)
 // router.post('/update',(req,res)=>{
   
 // })
