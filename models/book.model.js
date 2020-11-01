9a884baa5411d6e542b657a2d1ad8a960b620b92
Glitch (ls22-session2) <none>
2020-11-01 08:00:34 +0000

    🐸🍛 Checkpoint ./api/routes/user.route.js:20086806/357 ./api/routes/book.route.js:20086806/357 ./api/controllers/book.controller.js:20086806/1689 ./api/controllers/user.controller.js:20086806/568

--- models/book.model.js
+++ models/book.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var bookSchema = new mongoose.Schema({
     title: String,
     avatar: String,
-    description:String,
+    description:String
   
 })
 var Book = mongoose.model('Book',bookSchema)
